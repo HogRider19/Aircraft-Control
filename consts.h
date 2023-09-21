6ffd5302c78b63794b12b533aae10559c42d6832
Danil Dmitrenko <danilzzzg19@gmail.com>
2023-09-21 18:45:36 +0300

    Added consts for ZVER!

--- consts.h
+++ consts.h
@@ -1,2 +1,24 @@
 
-// Впить константы из методы через define
+// DNIT
+
+// GAVR
+
+// ZVER 4
+
+#define P 200000
+#define m_c 100
+#define c_xa0 0.4
+#define A 2.0
+#define c_ya_al 1.7
+#define m_z_wz -0.6
+#define m_z_dlb 0.8
+#define I_z 33000
+#define L 10
+#define S 0.78
+#define v_0 50
+#define th_0 1.55
+#define y_0 500
+#define m_0 440
+#define T 33
+#define h 0.5
+#define v_max 1542
