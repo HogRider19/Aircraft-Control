75d79fcd7a44342a6f11190e5a2776bae1c7899d
Danil Dmitrenko <danilzzzg19@gmail.com>
2023-09-24 16:24:15 +0300

    Small .h fix

--- solver.h
+++ solver.h
@@ -5,6 +5,7 @@
 typedef void (*SolverFunctionPtr)(double, double, double, const std::vector<double>&, std::vector<double>&);
 
 void systemOfEquations(double t, const std::vector<double>& y, std::vector<double>& results);
+SolverFunctionPtr getEnviconmentSolver();
 
 void rungeKuttaSystem(double t0, double t1, double step, const std::vector<double>& y0, std::vector<double>& y1);
 void eulerSystem(double t0, double t1, double step, const std::vector<double>& y0, std::vector<double>& y1);
