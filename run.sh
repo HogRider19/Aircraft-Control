7ca165d7c8016971d001fc02e84975a0ded6adb2
hogrider <danilzzzg19@gmail.com>
2023-10-22 13:33:54 +0300

    Added Linux support!

---
+++ run.sh
@@ -0,0 +1,14 @@
+g++ main.cpp \
+    src/Code/solver.cpp \
+    src/Code/equations.cpp \
+    src/Code/ploter.cpp \
+    \
+    -o main \
+    \
+    -I src/Include \
+    -I /usr/include/python3.10 \
+    -lpython3.10
+
+./main
+
+rm ./main
