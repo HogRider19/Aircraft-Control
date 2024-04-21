503a34886e5d244a885854d63912030d389f512d
Danil Dmitrenko <danilzzzg19@yandex.ru>
2024-04-21 17:18:34 +0300

    !

---
+++ Lb5_gav/run.bat
@@ -0,0 +1,8 @@
+
+@echo off
+
+g++ main.cpp -o main.exe -I /. -I C:\Users\79143\AppData\Local\Programs\Python\Python38\include -I C:\Users\79143\AppData\Local\Programs\Python\Python38\Lib\site-packages\numpy\core\include -L C:\Users\79143\AppData\Local\Programs\Python\Python38\libs -lpython38
+main.exe
+del main.exe
+
+pause
